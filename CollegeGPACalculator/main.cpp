3fe368d82a5209827b0b81e116744dbe8a9f2cf8
kmcatee95125 <kmcatee95125@gmail.com>
2020-09-27 14:05:49 -0700

    Initial Commit

---
+++ CollegeGPACalculator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CollegeGPACalculator
+//
+//  Created by katie on 9/27/20.
+//  Copyright © 2020 katie. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
