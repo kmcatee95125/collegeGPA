b5f6a4922658b7c6131754ef0ef27febd190e2b1
kmcatee95125 <kmcatee95125@gmail.com>
2020-09-28 16:43:36 -0700

    87 lines

--- CollegeGPACalculator/main.cpp
+++ CollegeGPACalculator/main.cpp
@@ -7,9 +7,81 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    char grade;
+    double gpa = 0.0;
+    int counter{0};
+    double finalGPA{0.0};
+    double credVal{0.0};
+    char sign;
+    
+    do {
+        
+    cout << "Enter a grade between A and F. Enter z if you have no more grades to enter.";
+    cin >> grade;
+    sign = cin.peek();
+    grade = toupper(grade);
+        if(grade == 'Z'){
+            cout << "Your final GPA is " << finalGPA << ".";
+            break;
+        }
+    switch(grade) {
+        case 'A':
+            gpa = 4.0;
+            counter++;
+            break;
+
+        case 'B':
+            gpa = 3.0;
+            counter++;
+            break;
+
+        case 'C':
+            gpa = 2.0;
+            counter++;
+            break;
+
+        case 'D':
+            gpa = 1.0;
+            counter++;
+            break;
+            
+        case'F':
+            gpa = 0.0;
+            counter++;
+            break;
+        default:
+            cout << "Invalid grade entered" << endl;
+            break;
+            
+        }
+        
+        switch (sign){
+            case '-':
+                gpa = gpa - 0.33;
+                break;
+            case '+':
+                gpa = gpa + 0.33;
+                break;
+        }
+        
+        double tester{0};
+           cout << "Enter the credit value for this course.";
+           cin >> tester;
+
+           if( tester >= 0.5 && tester <= 45.0){
+               credVal = tester + credVal;
+           }
+           else {
+               cout << "That's an invalid credit value. Please make sure your value is between 0.5 and 45.0" << endl;
+           }
+
+         
+        finalGPA = (credVal * gpa) / (counter * credVal);
+    }
+    while(grade != 'Z');
     return 0;
+
 }
