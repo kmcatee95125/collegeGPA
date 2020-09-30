618cb57a126db019cd0a28f83530cd15a0b87c40
kmcatee95125 <kmcatee95125@gmail.com>
2020-09-30 12:37:14 -0700

    new approach to - +

--- CollegeGPACalculator/main.cpp
+++ CollegeGPACalculator/main.cpp
@@ -11,77 +11,86 @@ using namespace std;
 
 int main(int argc, const char * argv[]) {
     char grade;
-    double gpa = 0.0;
-    int counter{0};
-    double finalGPA{0.0};
-    double credVal{0.0};
+    float gradePoint = 0.0f;
+    float finalCredits = 0.0f;
+    float finalGPA{0.0f};
+    float credVal{0.0f};
     char sign;
+    
     
     do {
         
     cout << "Enter a grade between A and F. Enter z if you have no more grades to enter.";
     cin >> grade;
     sign = cin.peek();
-    grade = toupper(grade);
-        if(grade == 'Z'){
-            cout << "Your final GPA is " << finalGPA << ".";
-            break;
-        }
+    grade = toupper( grade );
+        
+    if(grade == 'Z'){
+        break;
+    }
+        
     switch(grade) {
+        
         case 'A':
-            gpa = 4.0;
-            counter++;
+            gradePoint = 4.0f;
+            if(sign == '+'){
+                sign = '\n';
+                cout << "Changed A+ to an A. \n";
+            }
             break;
 
         case 'B':
-            gpa = 3.0;
-            counter++;
+            gradePoint = 3.0f;
             break;
 
         case 'C':
-            gpa = 2.0;
-            counter++;
+            gradePoint = 2.0f;
             break;
 
         case 'D':
-            gpa = 1.0;
-            counter++;
+            gradePoint = 1.0f;
             break;
             
-        case'F':
-            gpa = 0.0;
-            counter++;
+        case 'F':
+            gradePoint = 0.0f;
+            if(sign == '-'){
+                sign = '\n';
+                cout << "Changed F- to an F. \n";
+            }
             break;
         default:
-            cout << "Invalid grade entered" << endl;
+            cout << "Invalid grade entered\n" << endl;
             break;
             
         }
-        
-        switch (sign){
-            case '-':
-                gpa = gpa - 0.33;
-                break;
-            case '+':
-                gpa = gpa + 0.33;
-                break;
+    switch (sign){
+        case '-':
+            gradePoint = gradePoint - 0.33;
+            break;
+        case '+':
+            gradePoint = gradePoint + 0.33;
+            break;
+        case '\n':
+            break;
+        }
+       
+    float inputCred{0};
+        cout << "Enter the credit value for this course.";
+        cin >> inputCred;
+
+        if( inputCred >= 0.5 && inputCred <= 45.0){
+            credVal = inputCred + credVal;
+            finalCredits = finalCredits + (inputCred * gradePoint);
+        }
+        else {
+            cout << "That's an invalid credit value. Please make sure your value is between 0.5 and 45.0 \n" ;
         }
         
-        double tester{0};
-           cout << "Enter the credit value for this course.";
-           cin >> tester;
-
-           if( tester >= 0.5 && tester <= 45.0){
-               credVal = tester + credVal;
-           }
-           else {
-               cout << "That's an invalid credit value. Please make sure your value is between 0.5 and 45.0" << endl;
-           }
-
          
-        finalGPA = (credVal * gpa) / (counter * credVal);
+        
     }
     while(grade != 'Z');
-    return 0;
+    cout << "Your final GPA is: " << finalCredits / credVal << endl;
+    return finalGPA;
 
 }
